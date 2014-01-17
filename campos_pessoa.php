485e1cc52acd08210728512cb54d91a00232b116
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-17 14:49:58 -0200

    Create campos_pessoa.php

---
+++ campos_pessoa.php
@@ -0,0 +1,24 @@
+<label>Nome:</label>
+<input type = "text" name = "nome" />
+<br/>
+<input type="radio" name="sexo" value="m" checked="checked" />
+<label>Masculino</label>
+<input type="radio" name="sexo" value="f" />
+<label>Feminino</label>
+<br/>
+<input type="checkbox" name="aceito" value="sim" />
+<label>Aceito o contrato</label>
+<br/>
+<label>Estado</label>
+<select name="estado">
+    <option value ="-1">Selecione.....</option>
+    <option>Bahia</option>
+    <option>Sao Paulo</option>
+    <option>Rio de Janeiro</option>
+    
+</select>
+</br>
+<label>Observacoes : </label>
+<br/>
+<textarea name="observacoes"></textarea>
+<br/>
