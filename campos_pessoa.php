418b5625d690b4d619eb480a7bfc2b8b664b1870
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-17 18:37:54 -0200

    Update campos_pessoa.php

--- campos_pessoa.php
+++ campos_pessoa.php
@@ -1,6 +1,12 @@
 <label>Nome:</label>
 <input type = "text" name = "nome" />
 <br/>
+<label>Telefone:</label>
+<input type = "text" name = "telefone" />
+<br/>
+<label>Cpf:</label>
+<input type = "text" name = "cpf" />
+<br/>
 <input type="radio" name="sexo" value="m" checked="checked" />
 <label>Masculino</label>
 <input type="radio" name="sexo" value="f" />
